59712277a70f60e68aaa77612eb1424f2a964bc6
flyingbot91 <flyingbot91@gmx.com>
2018-11-12 23:33:38 +0100

    Added tests for A/V/T

--- tests/test_finder.py
+++ tests/test_finder.py
@@ -25,14 +25,56 @@ class ArgParserTestCase(TestCase):
         dummy_folders = [self.dummy_folder]
         parser = parse_args(dummy_folders)
         self.assertEqual(parser.folders, dummy_folders)
+        self.assertEqual(parser.audio, False)
+        self.assertEqual(parser.text, False)
+        self.assertEqual(parser.video, False)
 
         # Multiple readable folders are provided
         dummy_folders = [self.dummy_folder, os.path.join(self.dummy_folder, 'data')]
         parser = parse_args(dummy_folders)
         self.assertEqual(parser.folders, dummy_folders)
+        self.assertEqual(parser.audio, False)
+        self.assertEqual(parser.text, False)
+        self.assertEqual(parser.video, False)
 
-        # # Multiple folders are provided (some are not readable or do not exist)
-        # self.fail()
+    def test_parse_args__audio(self):
+        """Check argument 'audio' parsing"""
+
+        # No folders are provided
+        args = ['-a']
+        self.assertRaises(SystemExit, parse_args, args)
+
+        # Audio files set
+        args = [self.dummy_folder, '-a']
+        parser = parse_args(args)
+        self.assertEqual(parser.folders, args[0:1])
+        self.assertEqual(parser.audio, True)
+
+    def test_parse_args__text(self):
+        """Check argument 'text' parsing"""
+
+        # No folders are provided
+        args = ['-t']
+        self.assertRaises(SystemExit, parse_args, args)
+
+        # Text files set
+        args = [self.dummy_folder, '-t']
+        parser = parse_args(args)
+        self.assertEqual(parser.folders, args[0:1])
+        self.assertEqual(parser.text, True)
+
+    def test_parse_args__video(self):
+        """Check argument 'video' parsing"""
+
+        # No folders are provided
+        args = ['-v']
+        self.assertRaises(SystemExit, parse_args, args)
+
+        # Video files set
+        args = [self.dummy_folder, '-v']
+        parser = parse_args(args)
+        self.assertEqual(parser.folders, args[0:1])
+        self.assertEqual(parser.video, True)
 
 
 if __name__ == '__main__':
