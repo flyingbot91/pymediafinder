5ceec79cbd8d3609e1e4331c2820d8220589f3af
flyingbot91 <flyingbot91@gmx.com>
2018-11-12 23:23:12 +0100

    Filtering files by mediafile tracks (A/V/T)

--- src/finder.py
+++ src/finder.py
@@ -5,7 +5,9 @@
 import argparse
 import os
 import sys
+
 from glob import iglob
+from pymediainfo import MediaInfo
 
 
 def main(parser):
@@ -18,7 +20,15 @@ def main(parser):
     regex = '**/*.*'
     for folder in parser.folders:
         for filepath in iglob(os.path.join(folder, regex), recursive=True):
-            print(filepath)
+            if parser.audio or parser.text or parser.video:
+                media_info = MediaInfo.parse(filepath)
+                for track in media_info.tracks:
+                    if parser.audio and track.track_type == 'Audio':
+                        print("AUDIO:", filepath)
+                    if parser.text and track.track_type == 'Text':
+                        print("TEXT:", filepath)
+                    if parser.video and track.track_type == 'Video':
+                        print("VIDEO:", filepath)
 
 
 def parse_args(*args, **kwargs):
@@ -36,6 +46,21 @@ def parse_args(*args, **kwargs):
         nargs='+',
         #default='.',
         help='Folders where files will be searched'
+    )
+    parser.add_argument(
+        '-a', '--audio',
+        action='store_true',
+        help='Look for files containing audio tracks'
+    )
+    parser.add_argument(
+        '-v', '--video',
+        action='store_true',
+        help='Look for files containing video tracks'
+    )
+    parser.add_argument(
+        '-t', '--text',
+        action='store_true',
+        help='Look for files containing text tracks'
     )
     return parser.parse_args(*args, **kwargs)
 
