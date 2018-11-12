530748d39616e8ef76ead1ea4eb196ba390dda29
flyingbot91 <flyingbot91@gmx.com>
2018-11-12 22:49:16 +0100

    Added method to find files recursively in a list folders

--- src/finder.py
+++ src/finder.py
@@ -3,10 +3,32 @@
 
 """
 import argparse
+import os
 import sys
+from glob import iglob
+
+
+def main(parser):
+    """
+    Main method
+    :param parser:
+    :return:
+    """
+
+    regex = '**/*.*'
+    for folder in parser.folders:
+        for filepath in iglob(os.path.join(folder, regex), recursive=True):
+            print(filepath)
 
 
 def parse_args(*args, **kwargs):
+    """
+    Parse sys.argv[1:]
+    :param args:
+    :param kwargs:
+    :return:
+    """
+
     parser = argparse.ArgumentParser(description='Search parameters')
     parser.add_argument(
         'folders',
@@ -16,3 +38,8 @@ def parse_args(*args, **kwargs):
         help='Folders where files will be searched'
     )
     return parser.parse_args(*args, **kwargs)
+
+
+if __name__ == '__main__':
+    parser = parse_args(sys.argv[1:])
+    main(parser)
