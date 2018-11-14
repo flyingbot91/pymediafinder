f65a3fb41abd745322012210a0d52a15322a936d
flyingbot91 <flyingbot91@gmx.com>
2018-11-14 22:50:58 +0100

    Added 'setup.cfg'

---
+++ tests/src/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+"""
+
+__author__ = "flyingbot91"
+__copyright__ = "Copyright 2018"
+__date__ = "2018/11/14"
+__credits__ = ["flyingbot91"]
+__license__ = " GPLv3"
+__version__ = "0.0.1"
+__maintainer__ = "flyingbot91"
+__email__ = "flyingbot91@gmx.com"
+__status__ = "Development"
